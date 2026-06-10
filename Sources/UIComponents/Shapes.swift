4c30d3c7222333bc1aaa4728feca6180a5d03ab8
Athul T S <athulsethumadhavan@gmail.com>
2026-06-10 10:03:55 +0300

    fixes

--- Sources/UIComponents/Shapes.swift
+++ Sources/UIComponents/Shapes.swift
@@ -4,7 +4,7 @@ import SwiftUI
 public struct CustomCorner: Shape {
     var corners: UIRectCorner
     var radius: CGFloat
-    func path(in rect: CGRect) -> Path {
+    public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         
         return Path(path.cgPath)
