9d0fb06a8c2ddf10f0023181b962359f0f98b554
Athul T S <athulsethumadhavan@gmail.com>
2026-06-10 10:07:29 +0300

    add init as public for shapes

--- Sources/UIComponents/Shapes.swift
+++ Sources/UIComponents/Shapes.swift
@@ -4,6 +4,12 @@ import SwiftUI
 public struct CustomCorner: Shape {
     var corners: UIRectCorner
     var radius: CGFloat
+    
+    public init(corners: UIRectCorner, radius: CGFloat) {
+        self.corners = corners
+        self.radius = radius
+    }
+    
     public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         
